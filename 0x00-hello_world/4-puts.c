91267301612da474be8d448a3dcd5c68054dc337
Ahmed <bazubairu@st.ug.edu.gh>
2023-02-16 16:53:48 -0800

    work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: This program prints to standard output using put function
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
