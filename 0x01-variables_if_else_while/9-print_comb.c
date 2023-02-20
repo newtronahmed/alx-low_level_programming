f4f2d477b9432a660046e675842b32605b7d461d
Ahmed <bazubairu@st.ug.edu.gh>
2023-02-19 17:30:30 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,18 +6,14 @@
  */
 int main(void)
 {
-	int x;
 	int y;
 
-	for (x = 0; x < 10; x++)
+	for (y = 0; y < 10; y++)
 	{
-		for (y = 0; y < 10; y++)
-		{
-			putchar('0' + x);
-			putchar('0' + y);
-			putchar(',');
-			putchar(' ');
-		}
+		putchar('0' + y);
+		putchar(',');
+		putchar(' ');
+		putchar('\n');
 	}
 	return (0);
 }
