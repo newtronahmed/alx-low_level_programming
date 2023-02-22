1abb8b3db5a4fdc2b6451a659a3c374522ab81c6
Ahmed <bazubairu@st.ug.edu.gh>
2023-02-22 13:56:04 -0800

    work

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,18 @@ int print_sign(int n)
 {
 	if (n > 1)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		putchar(-1);
+		_putchar(-1);
 		return (-1);
 	}
+	_putchar('\n');
 }
