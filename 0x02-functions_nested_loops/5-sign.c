179d37f792e31fc87ff0be550a955cefe52569bd
Ahmed <bazubairu@st.ug.edu.gh>
2023-02-22 13:40:41 -0800

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - Print the sign on an input
+ *
+ * @n: Integer input
+ *
+ * Return: 1 or 0 or -1 depending on input
+ */
+int print_sign(int n)
+{
+	if (n > 1)
+	{
+		putchar("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar("0");
+		return (0);
+	}
+	else
+	{
+		putchar("-1");
+		return (-1);
+	}
+}
