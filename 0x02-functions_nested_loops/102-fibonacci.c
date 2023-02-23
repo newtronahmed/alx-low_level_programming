754784b141a94ded16037693d83fc572ce6b05ae
Ahmed <bazubairu@st.ug.edu.gh>
2023-02-23 15:46:47 -0800

    resoved

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,15 +14,16 @@ int main(void)
 	n1 = 1;
 	n2 = 2;
 
-	printf("%ld, \n", n1);
-	printf("%ld, \n", n2);
+	printf("%ld, ", n1);
+	printf("%ld, ", n2);
 	for (i = 3; i <= 50; i++)
 	{
 		cur = n1 + n2;
-		printf("%ld, \n", cur);
+		printf("%ld, ", cur);
 		n1 = n2;
 		n2 = cur;
 	}
+	printf("\n");
 	return (0);
 
 }
