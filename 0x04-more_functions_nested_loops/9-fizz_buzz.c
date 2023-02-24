e7c2d8d02a9e12ca95035469b780ff9c1d3550c4
Ahmed <bazubairu@st.ug.edu.gh>
2023-02-24 09:13:59 -0800

    work

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,16 @@ int main(void)
 {
 	int i;
 
-	printf("%d", 1);
-	for (i = 2; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
-			printf(" %s", "FizzBuzz");
+			printf("%s ", "FizzBuzz");
 		else if (i % 3 == 0)
-			printf(" %s","Fizz");
+			printf("%s ","Fizz");
 		else if (i % 5 == 0)
-			printf(" %s","Buzz");
+			printf("%s ","Buzz");
 		else
-			printf(" %d", i);
+			printf("%d ", i);
 	}
 	printf("\n");
 	return (0);
