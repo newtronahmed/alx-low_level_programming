7f1d84b8fe9c0418b6c750036aba6d6eeac6f730
Ahmed <bazubairu@st.ug.edu.gh>
2023-02-24 09:21:38 -0800

    worke

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,9 +15,9 @@ int main(void)
 		if ((i % 3 == 0) && (i % 5 == 0))
 			printf(" %s", "FizzBuzz");
 		else if (i % 3 == 0)
-			printf(" %s","Fizz");
+			printf(" %s", "Fizz");
 		else if (i % 5 == 0)
-			printf(" %s","Buzz");
+			printf(" %s", "Buzz");
 		else
 			printf(" %d", i);
 	}
