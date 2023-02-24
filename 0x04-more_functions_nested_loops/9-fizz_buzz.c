a697cd5ae83dc90ae6bb9a9b16f59b091ea2f6a3
Ahmed <bazubairu@st.ug.edu.gh>
2023-02-24 09:07:45 -0800

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - fizzbuzz challeng
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("%d", 1);
+	for (i = 2; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf(" %s", "FizzBuzz");
+		else if (i % 3 == 0)
+			printf(" %s","Fizz");
+		else if (i % 5 == 0)
+			printf(" %s","Buzz");
+		else
+			printf(" %d", i);
+	}
+	printf("\n");
+}
