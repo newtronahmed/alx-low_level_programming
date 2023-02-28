66278edae2ba05d8b21c31b0e5ed3d36c0932bd7
Ahmed <bazubairu@st.ug.edu.gh>
2023-02-27 18:32:17 -0800

    done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout.
+ * 
+ * @s: input parameter.
+ *
+ * Return: void
+ */
+
+void _puts(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i) != 0; i++)
+	{
+		putchar(*(s _ i));
+	}
+}
