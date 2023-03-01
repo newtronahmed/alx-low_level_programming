f88b9f84ce8a5993559b50027ea6d8392a485c8d
Ahmed <bazubairu@st.ug.edu.gh>
2023-02-28 20:22:45 -0800

    hmm

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,7 +3,7 @@
 /**
  * puts2 - prints every other character of a string,
  * starting with the first character
- * @st: input string.
+ * @str: input string.
  * Return: void
  */
 void puts2(char *str)
