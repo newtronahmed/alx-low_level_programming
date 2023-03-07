008b289d6c004d864ae69ddcafde47af0d2b709a
Ahmed <bazubairu@st.ug.edu.gh>
2023-03-06 16:24:35 -0800

    solved

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _memset - function fills the first n bytes of 
+ * _memset - function fills the first n bytes of
+ *
  * the memory area pointed to by s with the
  * constant byte b
  * @s: pointer
