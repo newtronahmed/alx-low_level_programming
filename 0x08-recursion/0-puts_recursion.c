2f2503ed33064088963f395d9c69e2cbbce82380
Ahmed <bazubairu@st.ug.edu.gh>
2023-03-09 06:57:44 -0800

    done

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string.
+ *
+ * @s: string input.
+ *
+ * Return: void.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(++s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#ifndef _MAIN_H__
+#define _MAIN_H_
+int _putchar(char);
+void _puts_recursion(char *s);
+#endif /* _MAIN_H_ */
