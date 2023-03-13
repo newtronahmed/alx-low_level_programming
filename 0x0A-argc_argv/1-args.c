a7f6183759d3cd41920a12190594142d1c9fb610
Ahmed <bazubairu@st.ug.edu.gh>
2023-03-13 09:41:14 -0700

    task 0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main -  prints the number of arguments
+ *
+ * @argc: argc.
+ * @argv: argv.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%d\n", argc);
+	return (0);
+}
