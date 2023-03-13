1975f0f925d0709ea766ba67135aff329f573e3d
Ahmed <bazubairu@st.ug.edu.gh>
2023-03-13 09:37:08 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main -  program that prints its name.
+ *
+ * @argc: argc.
+ * @argv: argv.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
