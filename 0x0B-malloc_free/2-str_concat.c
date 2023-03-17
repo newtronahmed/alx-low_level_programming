529052a0bdbe3c3172c9e1cc35499e1f49c42577
Ahmed <bazubairu@st.ug.edu.gh>
2023-03-16 18:29:42 -0700

    s2 not *s2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,9 +18,9 @@ char *str_concat(char *s1, char *s2)
 	int size = 0;
 
 	if (s1 == NULL)
-		*s1 = "";
+		s1 = "";
 	if (s2 == NULL)
-		*s2 = "";
+		s2 = "";
 	while (s1[count] != '\0')
 	{
 		count++;
