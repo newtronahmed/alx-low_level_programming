92b54aceb06dbd2f22c9ae96600aa074e91b2e1e
Ahmed <bazubairu@st.ug.edu.gh>
2023-03-20 20:52:00 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog structure
+ *
+ * @name: string
+ * @age: float
+ * @owner: string
+ *
+ * Description: Long description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
