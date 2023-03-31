34f47dec8c7406ec397af7ff4e5bc5b204c4c7ec
Ahmed <bazubairu@st.ug.edu.gh>
2023-03-31 09:35:29 -0700

    bs style

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -12,11 +12,10 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
-
 int _putchar(int c);
 size_t print_list(const list_t *h);
 #endif
