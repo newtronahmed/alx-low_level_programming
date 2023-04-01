fa37f3b2109997e6ac3b92732f44ebafda75c7e5
Ahmed <bazubairu@st.ug.edu.gh>
2023-03-31 20:12:53 -0700

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - frees memory of list
+ *
+ * @head: head
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}
