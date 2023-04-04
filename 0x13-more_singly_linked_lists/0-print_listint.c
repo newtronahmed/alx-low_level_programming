0243a17d2fbd91c61258e77e6e79407449192301
Ahmed <bazubairu@st.ug.edu.gh>
2023-04-03 20:16:22 -0700

    minor

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,7 +7,7 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count;
-	
+
 	count = 0;
 	while (h != NULL)
 	{
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newnode;
 
-	newnode= malloc(sizeof(listint_t));
+	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 	{
 		free(newnode);
