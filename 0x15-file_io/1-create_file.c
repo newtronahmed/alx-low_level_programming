35ce60ae150174ebaf7f5bc9d4c7c57e38f995ed
Ahmed <bazubairu@st.ug.edu.gh>
2023-04-09 08:54:27 -0700

    tsk 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,1 +1,31 @@
 #include "main.h"
+/**
+ * create_file - creates a file
+ *
+ * @filename: filename
+ * @text_content: text content
+ *
+ * Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+	}
+	if (bytes_written < 0)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
