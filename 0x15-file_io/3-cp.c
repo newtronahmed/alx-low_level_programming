7a71cc71855f552399d3e9850448bb346b2bbba8
Ahmed <bazubairu@st.ug.edu.gh>
2023-04-09 08:37:48 -0700

    betty errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,6 +20,20 @@ void close_file(int fd)
 		exit(100);
 	}
 }
+void cant_read(int fd_to, int fd_from, char *argv)
+{
+		close_file(fd_to);
+		close_file(fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv);
+		exit(98);
+
+}
+void cant_write(int fd_to, argv)
+{
+		close_file(fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv);
+		exit(99);
+}
 int main(int argc, char *argv[])
 {
 	int fd_to, fd_from;
@@ -31,42 +45,25 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd_from = open(argv[1], O_RDONLY);
-
 	if (fd_from < 0)
 	{
-		close_file(fd_from);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		cant_read(fd_from, fd_to, argv[1]);
 	}
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to < 0)
 	{
-		close_file(fd_to);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		cant_write(fd_to, argv[2]);
 	}
-	/* if both files successfully open, only then do we proceed */
 	if ((fd_from >= 0) && (fd_to >= 0))
 	{
-		/** read maximum of 1024 bytes
-		 * bytes_read is the number of bytes read
-		 * while bytes_read is greater than zero means
-		 * while we are'nt at the end of the file or 
-		 * while we didn't geta negative value which indicates
-		 * an error
-		 * Contunue to read to  buffer
-		 */
 		while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
 		{
 			bytes_written = write(fd_to, buffer, bytes_read);
 			if (bytes_written < 0 || bytes_written != bytes_read)
 			{
 				close_file(fd_from);
-				close_file(fd_to);
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-				exit(99);
+				cant_write(fd_to, argv[2]);
 			}
 		}
 
@@ -74,13 +71,9 @@ int main(int argc, char *argv[])
 
 	if (bytes_read < 0)
 	{
-		close_file(fd_from);
-		close_file(fd_to);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		cant_read(fd_from, fd_to, argv[1]);
 	}
 	close_file(fd_from);
 	close_file(fd_to);
-
 	return (0);
 }
