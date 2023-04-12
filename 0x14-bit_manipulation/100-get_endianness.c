b0304c35fb4ad2a960ae63be5e2802a7ee478a98
Ahmed <bazubairu@st.ug.edu.gh>
2023-04-11 17:56:31 -0700

    worked

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_endianness - checks for endianness
+ *
+ * Return: 0 or 1 
+ */
+int get_endianness(void)
+{
+	unsigned int y;
+	char *c;
+
+	y = 1;
+	c = (char *) &y;
+
+	return ((int)*c);
+}
