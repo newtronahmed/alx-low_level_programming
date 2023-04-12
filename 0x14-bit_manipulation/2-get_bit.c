4640563be87d05c2159da704e62526b0769710cb
Ahmed <bazubairu@st.ug.edu.gh>
2023-04-11 17:41:24 -0700

    stolen

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * get_bit -Returns: the value of the bit at index. 
+ * @n: input.
+ * @index: index of the bit.
+ *
+ * Return: value of bit.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (n == 0 && index < 64)
+		return (0);
+
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
+
+	return (-1);
+}
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0.
+ * @n: pointer
+ * @index: index of the bit.
+ *
+ * Return: 1 or -1 
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int temp;
+
+	if (index > 63)
+		return (-1);
+
+	temp = 1 << index;
+
+	if (*n & temp)
+		*n ^= temp;
+
+	return (1);
+}
