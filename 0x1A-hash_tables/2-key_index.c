55821f348d4fe77b5ba3ecb38ff2b07c23a97f5d
NeutronAhmed <bazubairu@st.ug.edu.gh>
2023-06-11 03:06:34 -0700

    hash tables.h

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "hash_tables.h"
 /**
  * key_index - a function that gives you the index of a key.
  *
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "hash_tables.h"
 /**
  * hash_table_set - adds an element to the hash table.
  *
